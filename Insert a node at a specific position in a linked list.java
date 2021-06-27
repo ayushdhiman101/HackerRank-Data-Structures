a5792ad6bf7af82d5edb482c1e46b50432bddf09
Ayush Dhiman <ayushdhiman101@gmail.com>
2021-06-27 22:42:05 +0530

    Create Insert a node at a specific position in a linked list.java

---
+++ Insert a node at a specific position in a linked list.java
@@ -0,0 +1,42 @@
+// Given the pointer to the head node of a linked list and an integer to insert at a certain position, create a new node with the given integer as its  attribute, insert this node at the desired position and return the head node.
+
+// A position of 0 indicates head, a position of 1 indicates one node away from the head and so on. The head pointer given may be null meaning that the initial list is empty.
+
+// Example
+//  refers to the first node in the list 
+
+
+// Insert a node at position  with . The new list is 
+
+// Function Description Complete the function insertNodeAtPosition in the editor below. It must return a reference to the head node of your finished list.
+
+// insertNodeAtPosition has the following parameters:
+
+// head: a SinglyLinkedListNode pointer to the head of the list
+// data: an integer value to insert as data in your new node
+// position: an integer position to insert the new node, zero based indexing
+// Returns
+
+// SinglyLinkedListNode pointer: a reference to the head of the revised list
+// Input Format
+
+// The first line contains an integer , the number of elements in the linked list.
+// Each of the next  lines contains an integer SinglyLinkedListNode[i].data.
+// The next line contains an integer , the data of the node that is to be inserted.
+// The last line contains an integer .
+public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+    // Write your code here
+    
+     SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        if(position==0){
+            node.next = llist.next;
+            llist=node;
+        }else{
+            SinglyLinkedListNode aux = new SinglyLinkedListNode(0);
+            aux.next = llist;
+            for(int i=0;i<position;i++)aux=aux.next;
+            node.next = aux.next;
+            aux.next=node;
+        }
+
+    return llist;
